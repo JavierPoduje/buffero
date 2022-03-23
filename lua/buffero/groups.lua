2ce35f74d2e444602cbc84053f37fd6c4febaa29
JavierPoduje <javier.poduje@gmail.com>
2022-03-23 16:32:36 -0300

    wip: fix

--- lua/buffero/groups.lua
+++ lua/buffero/groups.lua
@@ -1,13 +1,15 @@
 local S = require("buffero.state")
 
-local group = vim.api.nvim_create_augroup("Buffero", { clear = true })
+if vim.api['nvim_create_augroup'] ~= nil then
+  local group = vim.api.nvim_create_augroup("Buffero", { clear = true })
 
-vim.api.nvim_create_autocmd("BufDelete", {
-	group = group,
-	callback = function()
-		local data = { match = vim.fn.expand("<amatch>") }
-		vim.schedule(function()
-			table.insert(S.closed, data.match)
-		end)
-	end,
-})
+  vim.api.nvim_create_autocmd("BufDelete", {
+    group = group,
+    callback = function()
+      local data = { match = vim.fn.expand("<amatch>") }
+      vim.schedule(function()
+        table.insert(S.closed, data.match)
+      end)
+    end,
+  })
+end
